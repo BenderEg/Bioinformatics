0c268a6fb0697954db8948c2252c739ec238883e
Egor Nikitin <bender.bending.rodrigez@gmail.com>
2020-03-04 22:35:51 +0300

    add readme

--- GRAF.py
+++ GRAF.py
@@ -1,9 +1,25 @@
 def create_data(file_name):
     stats = []
     for line in open(file_name):
-        stats.append(line.strip().split(' '))
+        array = []
+        for ele in line.strip().split(' '):
+            if ele == '':
+                break
+            array.append(int(ele))
+        stats.append(array)
     return stats
 
 
-data = create_data('Input.txt')
-print(data)
+array = create_data('Input.txt')
+visited = [False]*int(array[0][0])
+array.remove(array[0])
+
+
+def dfs(number):
+    visited[number] = True
+    for ele in array[number]:
+        if not visited[ele]:
+            dfs(ele)
+    return visited.count(True)
+
+print(dfs(0))
